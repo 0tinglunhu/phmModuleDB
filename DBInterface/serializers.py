0beffeb96a8cbdcffb46ff8f57de62d89105b3f1
alantl_hu <Alantl_Hu@asus.com>
2022-08-23 21:01:01 +0800

    將HyperlinkedModelSerializer更換為ModelSerializer

--- DBInterface/serializers.py
+++ DBInterface/serializers.py
@@ -1,37 +1,37 @@
 from rest_framework import serializers
 from .models import Account, Field, Machine, Equlpment, Sensor, Physical_Sensor_manager
 
-class AccountSerializer(serializers.HyperlinkedModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model=Account
         #fields=('id', 'username', 'password', 'firstName', 'lastName', 'permission', 'userStatus')
         fields='__all__'
 
-class FieldSerializer(serializers.HyperlinkedModelSerializer):
+class FieldSerializer(serializers.ModelSerializer):
     class Meta:
         model=Field
         #fields=('id', 'name', 'buildingName', 'floor')
         fields='__all__'
 
-class MachineSerializer(serializers.HyperlinkedModelSerializer):
+class MachineSerializer(serializers.ModelSerializer):
     class Meta:
         model=Machine
         #fields=('id', 'name', 'brand', 'model', 'power', 'voltage', 'electric', 'photo', 'fieldId')
         fields='__all__'
 
-class EqulpmentSerializer(serializers.HyperlinkedModelSerializer):
+class EqulpmentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Equlpment
         #fields=('id', 'name', 'brand', 'model', 'power', 'material', 'frequencyType', 'rotationalSpeed', 'fladeCount', 'photo', 'machineId')
         fields='__all__'
 
-class SensorSerializer(serializers.HyperlinkedModelSerializer):
+class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model=Sensor
         #fields=('id', 'cycle', 'frequency', 'installLocation', 'equipmentId', 'physicalSensorId')
         fields='__all__'
 
-class PhysicalSensorManagerSerializer(serializers.HyperlinkedModelSerializer):
+class PhysicalSensorManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model=Physical_Sensor_manager
         #fields=('id', 'serialNumber', 'name', 'model', 'brand', 'battery', 'wifi', 'cycle', 'frequency', 'onlineTime', 'lastUploadDataTime')
